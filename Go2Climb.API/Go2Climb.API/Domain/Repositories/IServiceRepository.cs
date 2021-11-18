f49acc05b1aead9bbe11c88966ef7cd0a300e39b
Heber Cordova <u201924464@upc.edu.pe>
2021-11-18 17:03:21 -0500

    feat: Service search filters implemented
    The search for score and service cost was implemented.

--- Go2Climb.API/Go2Climb.API/Domain/Repositories/IServiceRepository.cs
+++ Go2Climb.API/Go2Climb.API/Domain/Repositories/IServiceRepository.cs
@@ -9,6 +9,9 @@ namespace Go2Climb.API.Domain.Repositories
         Task<IEnumerable<Service>> ListAsync();
         Task<IEnumerable<Service>> ListByAgencyId(int agencyId);
         Task<IEnumerable<Service>> ListByText(string name, int start, int limit);
+        Task<IEnumerable<Service>> ListByTextFilterMoney(string name, int min, int max, int start, int limit);
+        Task<IEnumerable<Service>> ListByTextFilterScore(string name, int score, int start, int limit);
+        Task<IEnumerable<Service>> ListByTextAndAllFilter(string name, int score, int min, int max, int start, int limit);
         Task<IEnumerable<Service>> ListById(int id);
         Task<Service> FindById(int id);
         Task AddAsync(Service service);
